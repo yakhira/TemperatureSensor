89d3259dd8d604a4bf2f9fa7b2c26acdb05c74c4
yakhira <ruslan.k.yakhin@gmail.com>
2026-01-21 13:57:29 +0100

    Moved to blynk

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,30 @@
-#include "setup/wifi.h"
+#define BLYNK_TEMPLATE_ID "TMPL4nRADNRad"
+#define BLYNK_TEMPLATE_NAME "Temperature Sensor"
+#define BOARD_BUTTON_PIN 5
+#define BLYNK_FIRMWARE_VERSION        "0.1.0"
+#define BLYNK_PRINT Serial
+#define APP_DEBUG
+#define BLYNK_DEBUG
+
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <Wire.h>
+#include "BlynkEdgent.h"
 
 #define BMP280_ADDRESS 0x76
 #define BMP280_POWER_PIN 14
 #define BMP280_SDA_PIN 12
 #define BMP280_SCL_PIN 13
 
-// -------- DEFAULT SKETCH PARAMETERS --------
-const int SKETCH_VERSION = 11; 
+void main_code()
+{
+    Serial.println("Reading BMP280 sensor data...");
+    Adafruit_BMP280 bmp;
 
-ESPWiFi espwifi("ESP12-F");
+    digitalWrite(BMP280_POWER_PIN, HIGH);
 
-void main_code(){
-	JSONVar data;
-	Adafruit_BMP280 bmp;
-
-	pinMode(BMP280_POWER_PIN, OUTPUT);
-	digitalWrite(BMP280_POWER_PIN, HIGH);
-
-	delay(3000);
-
-	espwifi.readFile("bmp.txt", data);
-
-	Wire.begin(BMP280_SDA_PIN, BMP280_SCL_PIN);
+    delay(3000);
+    Wire.begin(BMP280_SDA_PIN, BMP280_SCL_PIN);
 
     if (bmp.begin(BMP280_ADDRESS)) {
 		bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, /* Operating Mode. */
@@ -33,47 +33,41 @@ void main_code(){
 					Adafruit_BMP280::FILTER_X16,      /* Filtering. */
 					Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
-		String temperature = String(bmp.readTemperature(), 1);
-		String pressure =  String(bmp.readPressure() / 100, 1);
+        float temperature = bmp.readTemperature();
+        float pressure = bmp.readPressure() / 100;
 
-		temperature.replace(".", ",");
-		pressure.replace(".", ",");
+        if (!isnan(temperature) && !isnan(pressure))
+        {
+            Serial.println("Sending BMP280 sensor data to Blynk...");
+            Serial.print("Temperature = "); Serial.print(temperature);
+            Serial.println(" *C");
+            Serial.print("Pressure = "); Serial.print(pressure);
+            Serial.println(" hPa");
 
-		if ((String)temperature != data["temperature"] || (String)pressure != data["pressure"]){
-			String dataUrl = espwifi.dataUrl + "/esp/temperature?mac=" + WiFi.macAddress();
+            Blynk.virtualWrite(V0, temperature);
+            Blynk.virtualWrite(V1, pressure);
 
-			if (espwifi.sendHTTPJsonData(dataUrl, data)) {
-				data["temperature"] = temperature; 
-				data["pressure"] = pressure;
-				espwifi.saveFile("bmp.txt", data);
-			} else {
-				Serial.println("Could not send data to " + dataUrl + "!");
-			}
-		}
-	} else {
-		Serial.println("Could not find a valid BME280 sensor, check wiring, address, sensor ID!");
-	}
+            Serial.println("Sent BMP280 sensor data...");
+        }
+    }
+    else {
+        Serial.println("Could not find a valid BMP280 sensor, check wiring, address, sensor ID!");
+    }
 
-	digitalWrite(BMP280_POWER_PIN, LOW);
-	ESP.deepSleepInstant(3600e6);
+    digitalWrite(BMP280_POWER_PIN, LOW);
+    ESP.deepSleepInstant(3600e6);
 }
 
-void setup() {
-	Serial.begin(74880);
-	espwifi.wifiConnect();
+void setup()
+{
+    Serial.begin(74880);
+    delay(100);
 
-	if (WiFi.getMode() == WIFI_STA){
-		espwifi.updateSketch(SKETCH_VERSION);
-		main_code();
-	}
+    pinMode(BMP280_POWER_PIN, OUTPUT);
+    BlynkEdgent.begin();
 }
 
 void loop() {
-	if (WiFi.getMode() == WIFI_STA) {
-		if (WiFi.status() != WL_CONNECTED) {
-			WiFi.reconnect();
-		}
-	}
-
-	espwifi.stateCheck();
+  BlynkEdgent.run();
+  if (Blynk.connected()) main_code();
 }
